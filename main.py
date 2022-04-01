fb7f8bbd3620b18a6ae8c1ece87c7541739d7943
vinicius almeida <87548413+pereirafunk448@users.noreply.github.com>
2022-04-01 00:28:15 +0000

    estilizado

--- main.py
+++ main.py
@@ -2,21 +2,48 @@ from flask import Flask, render_template, request, redirect
 app = Flask('app')
 
 todos = [
-  {'title': 'Estudar Python'},
-  {'title': 'Estudar JavaScript'}  
+  { 
+    'title': 'Estudar Python',
+    'complete': False
+  },
+  { 
+    'title': 'Estudar JavaScript',
+    'complete': True
+  }
 ]
 
 @app.route('/')
-def hello_world():
+def index():
   return render_template(
     'index.html',
     todos=todos
   )
+
 @app.route('/create', methods=['POST'])
 def create():
   title = request.form.get('title')
-  todos.append({'title' : title})
+  todos.append({
+    'title': title,
+    'complete': False
+  })
   return redirect('/')
 
+@app.route('/delete/<int:index>')
+def delete(index):
+  todos.pop(index)
+  return redirect('/')
+
+@app.route('/complete/<int:index>')
+def complete(index):
+  todos[index]['complete'] = True
+  return redirect('/')
+
+@app.route('/update/<int:index>', methods=['POST'])
+def update(index):
+  title = request.form.get('title')
+  todos[index]['title'] = title
+  return redirect('/')
+  
+# IMPORTANTE V
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080)
